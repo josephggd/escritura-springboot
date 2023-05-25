5556cde03fa48cf5404e3d688d66a7536eddc351
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2023-05-24 23:31:19 -0500

    Initial commit

---
+++ src/main/java/com/kobe2/escritura/EscrituraApplication.java
@@ -0,0 +1,13 @@
+package com.kobe2.escritura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscrituraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscrituraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kobe2/escritura/EscrituraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobe2.escritura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscrituraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
